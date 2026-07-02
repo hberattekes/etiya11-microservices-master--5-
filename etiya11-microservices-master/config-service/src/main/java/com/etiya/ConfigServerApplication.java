76599b232bbf6d9aa9b908a8c5cdb6e2f06de64c
Abdullah Mert Şahin <mert.sahin@etiya.com>
2026-07-02 11:36:03 +0300

    yaml confi

--- etiya11-microservices-master/config-service/src/main/java/com/etiya/ConfigServerApplication.java
+++ etiya11-microservices-master/config-service/src/main/java/com/etiya/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package com.etiya;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
-    static void main() {
-
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
