e40650ece65b087b98b9aee0a7849f708853a1dd
Abdullah Mert Şahin <mert.sahin@etiya.com>
2026-07-02 11:17:28 +0300

    mert commit

---
+++ etiya11-microservices-master/config-service/src/main/java/com/etiya/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.etiya;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    static void main() {
+
+    }
+}
